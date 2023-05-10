ba751a78da5a7627b2fbf11f1b1af6e3f5bedb25
Tulasidas <dasbiradar1999@gamil.com>
2023-05-10 23:52:46 +0530

    added example for linkedlist

--- LinkedList/displayLinkedList.cpp
+++ LinkedList/displayLinkedList.cpp
@@ -44,6 +44,58 @@ int maxElementUsingLoop(struct Node *tempPointer){
     }
     return max;
 }
+struct Node* searchElementInLinkedList(struct Node *tempPointer, int element){
+    while(tempPointer!=NULL){
+        if(tempPointer->data == element){
+            return tempPointer;
+        }
+        tempPointer = tempPointer->pointer;
+    }
+    return 0;
+}
+void insertIntoALinkedList(struct Node *tempPointer, int position, int element){
+    if(position == 0){
+    struct Node *atZero;
+    struct Node *pointingPointer=tempPointer;
+     atZero = (struct Node *)malloc(sizeof(struct Node));
+    atZero->data = element;
+    atZero->pointer = pointingPointer;
+    pointingPointer = atZero;
+    }else if(position>0){
+        struct Node *atPosition;
+        struct Node *pointingPointer;
+        pointingPointer = tempPointer;
+        atPosition = (struct Node *)malloc(sizeof(struct Node));
+        atPosition->data = element;
+        for(int i=0;i<position-1&&pointingPointer;i++){
+            pointingPointer=pointingPointer->pointer;
+        }
+        if(pointingPointer){
+        atPosition->pointer = pointingPointer->pointer;
+        pointingPointer->pointer = atPosition;
+        }
+    }
+}
+void insertingInASortedLinkedList(int element, struct Node *tempPointer){
+    struct Node *nodeForElement;
+    nodeForElement = (struct Node *)malloc(sizeof(struct Node));
+    nodeForElement->data = element;
+    nodeForElement->pointer = NULL;
+    struct Node *firstPointer;
+    firstPointer = NULL;
+    struct Node *secondPointer = tempPointer;
+    if(secondPointer==NULL){
+        secondPointer = nodeForElement;
+    }else{
+        while((secondPointer!=NULL) && (secondPointer->data<nodeForElement->data)){
+            firstPointer = secondPointer;
+            secondPointer = secondPointer->pointer;
+        }
+        nodeForElement->pointer = secondPointer;
+        firstPointer->pointer = nodeForElement;
+    }
+
+}
 int main(){
     struct Node *first;
     struct Node *second;
@@ -54,10 +106,22 @@ int main(){
     second->data = 10;
     second->pointer = NULL;
     display(first);
-   cout<<"The number of nodes avaiale in the linked list are "<<countNodes(first)<<endl;
+    cout<<"The number of nodes avaiale in the linked list are "<<countNodes(first)<<endl;
     cout<<"The sum of all the elements in node are "<<sumOfElementsInLinkedList(first)<<endl;
     cout<<"Maximum element using recursions: "<<maxElemementInLinkedList(first)<<endl;
     cout<<"Maximum element using loops: "<<maxElementUsingLoop(first)<<endl;
+    cout<<"The element found at: "<< searchElementInLinkedList(first,5)<<endl;
+    insertIntoALinkedList(first,1,19);
+    insertIntoALinkedList(first, 0, 20);
+    display(first);
+
+    struct Node *sortedPointer;
+    sortedPointer = (struct Node *)malloc(sizeof(struct Node));
+    sortedPointer->data = 5;
+    sortedPointer->pointer = NULL;
+    insertingInASortedLinkedList(6, sortedPointer);
+    insertingInASortedLinkedList(7,sortedPointer);
+    display(sortedPointer);
     return 0;
 
 }
